b73c80bb31d4cc05dc8fa98b2fce2eef9a7c47ba
yehea <yeheafandi@gmail.com>
2026-07-16 14:06:22 +0300

    fix(routes.dart): resolve routes conflict and finalize search view implementation

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,9 +4,6 @@ class Routes{
 
   static const String homeview = '/homeview';
   static const String nearbyMap = '/nearby-map';
-<<<<<<< HEAD
-=======
   static const String search = '/search';
->>>>>>> feat/home-view
 
 }

caf87679e29c2153f9bb958c63c68f16ebd69ba1
yehea <yeheafandi@gmail.com>
2026-07-16 14:34:01 +0300

    merge: resolve final icon circle button conflict on main

--- lib/widgets/icon_circle_button.dart
+++ lib/widgets/icon_circle_button.dart
@@ -1,22 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import '../../core/constants/app_colors.dart';
-
-class IconCircleButton extends StatelessWidget {
-  final IconData icon;
-  final VoidCallback onTap;
-  final bool hasCircle; 
-  final double size;    
-  final double iconSize;  
-
-  const IconCircleButton({
-    super.key,
-    required this.icon,
-    required this.onTap,
-    this.hasCircle = true,
-    this.size = 42.0,     
-    this.iconSize = 18.0,  
-=======
 import 'package:focal_project/core/constants/app_colors.dart';
 
 class CustomCircleIconButton extends StatelessWidget {
@@ -39,36 +21,10 @@ class CustomCircleIconButton extends StatelessWidget {
     this.backgroundColor = AppColors.primaryWhite,
     this.hasBorder = false,
     this.hasShadow = false,
->>>>>>> feat/home-view
   });
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: hasCircle ? size : null, 
-        height: hasCircle ? size : null,
-        alignment: Alignment.center, 
-        decoration: hasCircle
-            ? BoxDecoration(
-                color: AppColors.primaryWhite,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: AppColors.primaryBlack, 
-                    blurRadius: 10,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              )
-            : null, 
-        child: Icon(
-          icon, 
-          color: AppColors.primaryBlack, 
-          size: iconSize,
-=======
     return Container(
       width: size,
       height: size,
@@ -92,7 +48,6 @@ class CustomCircleIconButton extends StatelessWidget {
           constraints: const BoxConstraints(),
           icon: Icon(icon, color: iconColor, size: iconSize),
           onPressed: onPressed,
->>>>>>> feat/home-view
         ),
       ),
     );

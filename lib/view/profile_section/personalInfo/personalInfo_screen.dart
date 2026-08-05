f2d9127e116394a96593f17906e6577452cad488
Jan Massouh <janmassouh42@gmail.com>
2026-08-05 20:35:29 +0300

    (fix):ui page alignment

--- lib/view/profile_section/personalInfo/personalInfo_screen.dart
+++ lib/view/profile_section/personalInfo/personalInfo_screen.dart
@@ -19,86 +19,90 @@ class PersonalInfoScreen extends GetView<PersonalInfoController> {
     return Scaffold(
       backgroundColor: AppColors.primaryWhite,
 
-      body: Padding(
+      body: ListView(
         padding: EdgeInsets.symmetric(horizontal: AppSpaces.widthLarge),
-        child: Column(
-          children: [
-            CustomAppBar(
-              title: 'personal_info'.tr,
-              action: GestureDetector(
-                onTap: controller.toggleEditing,
-                child: SvgPicture.asset(AppIcons.editIcon),
-              ),
+        children: [
+          CustomAppBar(
+            title: 'personal_info'.tr,
+            action: GestureDetector(
+              onTap: controller.toggleEditing,
+              child: SvgPicture.asset(AppIcons.editIcon),
             ),
-            SizedBox(height: AppSpaces.heightLarge),
-            Obx(
-              () => Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  LabeledTextField(
-                    label: 'first_name'.tr,
-                    controller: controller.firstNameCtrl,
-                    enabled: controller.isEditing.value,
-                  ),
-                  SizedBox(height: AppSpaces.heightNormal),
-                  LabeledTextField(
-                    label: 'last_name'.tr,
-                    controller: controller.lastNameCtrl,
-                    enabled: controller.isEditing.value,
-                  ),
-                  SizedBox(height: AppSpaces.heightNormal),
-                  LabeledTextField(
-                    label: 'email'.tr,
-                    controller: controller.emailCtrl,
-                    enabled: controller.isEditing.value,
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  SizedBox(height: AppSpaces.heightNormal),
-                  LabeledTextField(
-                    label: 'phone'.tr,
-                    controller: controller.phoneCtrl,
-                    enabled: controller.isEditing.value,
-                    keyboardType: TextInputType.phone,
-                  ),
-                  SizedBox(height: 56),
-                  // زر Save Changes - بيصير Active (أزرق) بس بوضع التعديل
-                  SizedBox(height: 56,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: controller.isEditing.value
-                          ? controller.saveChanges
-                          : null,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: controller.isEditing.value
-                            ? AppColors.primaryBlue
-                            : AppColors.formFiled,
-                        disabledBackgroundColor: AppColors.formFiled,
-                        elevation: 0,
-                        padding: EdgeInsets.symmetric(
-                          vertical: AppSpaces.paddingMedium,
+          ),
+          SizedBox(height: AppSpaces.heightLarge),
+          Obx(
+            () => Column(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    LabeledTextField(
+                      label: 'first_name'.tr,
+                      controller: controller.firstNameCtrl,
+                      enabled: controller.isEditing.value,
+                    ),
+                    SizedBox(height: AppSpaces.heightNormal),
+                    LabeledTextField(
+                      label: 'last_name'.tr,
+                      controller: controller.lastNameCtrl,
+                      enabled: controller.isEditing.value,
+                    ),
+                    SizedBox(height: AppSpaces.heightNormal),
+                    LabeledTextField(
+                      label: 'email'.tr,
+                      controller: controller.emailCtrl,
+                      enabled: controller.isEditing.value,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    SizedBox(height: AppSpaces.heightNormal),
+                    LabeledTextField(
+                      label: 'phone'.tr,
+                      controller: controller.phoneCtrl,
+                      enabled: controller.isEditing.value,
+                      keyboardType: TextInputType.phone,
+                    ),
+                    SizedBox(height: 56),
+                    // زر Save Changes - بيصير Active (أزرق) بس بوضع التعديل
+                    SizedBox(
+                      height: 56,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: controller.isEditing.value
+                            ? controller.saveChanges
+                            : null,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: controller.isEditing.value
+                              ? AppColors.primaryBlue
+                              : AppColors.formFiled,
+                          disabledBackgroundColor: AppColors.formFiled,
+                          elevation: 0,
+                          padding: EdgeInsets.symmetric(
+                            vertical: AppSpaces.paddingMedium,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              AppSpaces.radiusMedium,
+                            ),
+                          ),
                         ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                            AppSpaces.radiusMedium,
+                        child: Text(
+                          'save_changes'.tr,
+                          style: MyTextStyle.normalTitleText(
+                            color: controller.isEditing.value
+                                ? AppColors.primaryWhite
+                                : AppColors.grey400,
+                            size: 16,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
                       ),
-                      child: Text(
-                        'save_changes'.tr,
-                        style: MyTextStyle.normalTitleText(
-                          color: controller.isEditing.value
-                              ? AppColors.primaryWhite
-                              : AppColors.grey400,
-                          size: 16,fontWeight: FontWeight.w500
-                        ),
-                      ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
